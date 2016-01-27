efa0c8a1f50d1b43d6a8b1150e70d7db6c380037
julesx81 <lorenzo.scattolin04@libero.it>
2016-01-27 21:40:54 +0100

    moved on src

---
+++ src/siteadmappspese/settings_private.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'owqe_jehrguggrel:As5@ymcgm$zbr%c45_!p1m%46z6q_4@r'
+
+DATABASES = {
+    'default': {
+        'NAME': 'appspesedb',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'psLorenzo',
+        'PASSWORD': 'apkdao_QCK&81',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
